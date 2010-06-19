712ef4209d74175c0fed1b8ab00150ca673fca63
Seth Pellegrino <seth.pellegrino@gmail.com>
2010-06-19 09:16:40 -0700

    Created a ScheduleManager

--- src/java/persistance/schedule/FileSchedule.java
+++ src/java/persistance/schedule/FileSchedule.java
@@ -1,7 +1,6 @@
 package persistance.schedule;
 
 import java.io.File;
-import java.net.URISyntaxException;
 import java.util.ArrayList;
 
 import persistance.schedule.store.FileParser;
@@ -9,38 +8,7 @@ import persistance.schedule.store.FileParser;
 /** Loads and stores the Raz schedule. */
 public class FileSchedule implements Schedule {
 	
-	/** The schedule file, for when we're not bundled as a jar */
-	public static final String SCHEDULES_FILE = "resources" + File.separator + "schedule.txt";
-	
-	/** The URI for the schedule resource. Always always always use '/' in URIs. */
-	public static final String SCHEDULES_RESOURCE = "/resources/schedule.txt";
-	
-	private static final Schedule theSchedules;
-	
-	/** We try to find/load the schedules file */
-	static {
-		// First, see if it's in the local filesystem (i.e. running from eclipse)
-		File file = new File(SCHEDULES_FILE);
-		if(!file.canRead()) {
-			// Otherwise, look for it somewhere the classloader can get at it
-			// i.e., if we're running from a JAR
-			try {
-				file = new File(FileSchedule.class.getResource(SCHEDULES_RESOURCE).toURI());
-			} catch (URISyntaxException e) {
-				e.printStackTrace();
-				System.exit(1);
-			}
-		}
-		theSchedules = new FileSchedule(file);
-	}
-
-	/** Returns the one and only list of schedules  */
-	public static Schedule getSchedules() {
-		return theSchedules;
-	}
-	
-	private FileSchedule(File schedule) {
-		// TODO: Store this, or maybe these? (i.e. time based structure and stop-based?)
+	public FileSchedule(File schedule) {
 		new FileParser(schedule);
 	}
 	
