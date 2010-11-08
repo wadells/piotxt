e1389b69ec2d14a603aa39a7aa93098f49e68521
Walt Javins <walt@javins.net>
2010-11-08 02:01:41 -0800

    Added semicoherent output.
    Piotext now prints some things to system out.  As well, there is
    a verbose property that can be set.  It defaults to false.
    
    Probably shouldn't have, but also organized the methods in PioText.

--- src/java/core/PioText.java
+++ src/java/core/PioText.java
@@ -4,6 +4,7 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.HashSet;
 import java.util.List;
@@ -22,6 +23,9 @@ public class PioText {
 
 	public final static String VERSION = "piotxt-v0.5";
 
+	/** How often PioText will check for new messages, in ms. */
+	final static long CHECK_FREQ = 60000;
+
 	/** The file where setup information is stored. */
 	public final static File PROPERTY_FILE = new File(
 			"resources/pio_text.properties");
@@ -30,26 +34,18 @@ public class PioText {
 	public final static File SECURE_PROPERTY_FILE = new File(
 			"resources/secure.properties");
 
-	/** How often PioText will check for new messages. */
-	final static long CHECK_FREQ = 60000;
+	/** The format for printing dates when running in verbose mode */
+	public final static SimpleDateFormat SYSOUT_FORMAT = new SimpleDateFormat(
+			"MM/d h:mm a");
 
-	/**
-	 * The number of time PioText will try to retrieve new messages before
-	 * giving up.
-	 */
-	final static long RECIEVE_RETRY = 3;
-
-	/**
-	 * The number of time PioText will try to send response messages before
-	 * giving up.
-	 */
-	final static long SEND_RETRY = 3;
-
-	/** The message handler that will process queries. */
-	private MessageHandler handler;
+	/** Whether or not PioText should output basic data to the console. */
+	private static boolean verbose;
 
 	/** The connection to send and recieve sms through */
 	private SmsConnection connection;
+
+	/** The message handler that will process queries. */
+	private MessageHandler handler;
 
 	/**
 	 * Creates a new PioText with the given properties.
@@ -60,7 +56,6 @@ public class PioText {
 		String gvUsername = props.getProperty("gv_user");
 		String gvPassword = props.getProperty("gv_pass");
 		connection = new GvConnection(gvUsername, gvPassword);
-
 		try {
 			handler = (MessageHandler) Class.forName(
 					props.getProperty("message_handler")).newInstance();
@@ -73,33 +68,83 @@ public class PioText {
 		handler.intialize(props);
 	}
 
-	public static void main(String[] args) {
-		// load properties
-		Properties props = null;
-		try {
-			props = load(PROPERTY_FILE, SECURE_PROPERTY_FILE);
-		} catch (IOException e) {
-			System.err.println("Could load properties at " + PROPERTY_FILE
-					+ ".  Exiting...");
-			e.printStackTrace();
-			System.exit(1);
-		}
-		System.out.println("Sucessfully loaded properties!");
+	/** Initializes the sms connection. */
+	public void initialize() throws ConnectionException {
+		connection.connect();
+	}
 
-		// set up pio text
-		PioText piotxt = new PioText(props);
-		try {
-			piotxt.initialize();
-		} catch (ConnectionException e) {
-			System.out
-					.println("Could not establish sms connection. Exiting...");
-			e.printStackTrace();
-			System.exit(1);
-		}
-		System.out.println("Sucessfully initialized PioText.");
+	/** @return true if PioText is running in verbose mode. */
+	public boolean isVerbose() {
+		return verbose;
+	}
 
-		// run
-		piotxt.run();
+	/**
+	 * Generates a reply to the query, sends the reply, deletes the query from
+	 * the server, and then logs the query. This can fail, and will throw an
+	 * exception if it does.
+	 * 
+	 * @param query
+	 *            the query to be processed
+	 */
+	private void processQuery(Query query) throws ConnectionException {
+		String response = handler.getResponse(query);
+		connection.sendSms(query.getPhoneNumber(), response);
+		query.setTimeResponded(new Date());
+		query.setResponse(response);
+		connection.deleteSms(query);
+		log(query);
+
+	}
+
+	public void run() {
+		System.out.println("PioText is running...");
+		if (verbose) {
+			System.out.println("-----------------------------------");
+		}
+		HashSet<Query> processed = new HashSet<Query>();
+		List<? extends Query> queries;
+		boolean firstTime = true;
+		while (true) {
+			try {
+				queries = connection.getNewMessages();
+				int newQueries = 0;
+				for (Query q : queries) {
+					if (!processed.contains(q)) {
+						newQueries++;
+						try {
+							processQuery(q);
+							processed.add(q);
+						} catch (ConnectionException e) {
+							// TODO: retry/system log this
+							e.printStackTrace();
+						}
+					}
+				}
+				if (verbose && (newQueries > 0 || firstTime)) {
+					// TODO: system log this with timestamp
+					System.out
+							.printf("%-20s", SYSOUT_FORMAT.format(new Date()));
+					String plural = "queries.";
+					if (newQueries == 1)
+						plural = "query.";
+					System.out.printf("%2d new %-7s\n", newQueries, plural);
+					firstTime = false;
+				}
+			} catch (ConnectionException e) {
+				// TODO system log this
+				e.printStackTrace();
+			}
+			// sleep for a while before cheking again
+			try {
+				Thread.sleep(CHECK_FREQ);
+			} catch (InterruptedException e) {
+				System.out
+						.println("Something has gone terribly wrong, and the PioText thread has been interrupted. Exiting...");
+				e.printStackTrace();
+				System.exit(1);
+			}
+
+		}
 	}
 
 	/**
@@ -122,67 +167,50 @@ public class PioText {
 		return result;
 	}
 
-	public void run() {
-		HashSet<Query> processed = new HashSet<Query>();
-		List<? extends Query> queries;
-		while (true) {
-			try {
-				queries = connection.getNewMessages();
-				int newQueries = 0;
-				for (Query q : queries) {
-					if (!processed.contains(q)) {
-						newQueries++;
-						try {
-							processQuery(q);
-							processed.add(q);
-						} catch (ConnectionException e) {
-							// TODO: retry/system log this
-							e.printStackTrace();
-						}
-					}
-				}
-				if (newQueries > 0) {
-					// TODO: system log this with timestamp
-					System.out.println(newQueries + " new queries.");
-				}
-			} catch (ConnectionException e) {
-				// TODO system log this
-				e.printStackTrace();
-			}
-			// sleep for a while before cheking again
-			try {
-				Thread.sleep(CHECK_FREQ);
-			} catch (InterruptedException e) {
-				System.out
-						.println("Something has gone terribly wrong, and the PioText thread has been interrupted. Exiting...");
-				e.printStackTrace();
-				System.exit(1);
-			}
+	public static void main(String[] args) {
+		// Say Hi!
+		System.out.println("PioText Sms Server");
+		System.out.println("-----------------------------------");
 
+		// load properties
+		System.out.printf("%-30s", "Loading properties...");
+		Properties props = null;
+		try {
+			props = load(PROPERTY_FILE, SECURE_PROPERTY_FILE);
+		} catch (IOException e) {
+			System.err.println("Could load properties at " + PROPERTY_FILE
+					+ ".  Exiting...");
+			e.printStackTrace();
+			System.exit(1);
 		}
-	}
+		System.out.println("Done.");
 
-	/**
-	 * Generates a reply to the query, sends the reply, deletes the query from
-	 * the server, and then logs the query. This can fail, and will throw an
-	 * exception if it does.
-	 * 
-	 * @param query
-	 *            the query to be processed
-	 */
-	private void processQuery(Query query) throws ConnectionException {
-		String response = handler.getResponse(query);
-		connection.sendSms(query.getPhoneNumber(), response);
-		query.setTimeResponded(new Date());
-		query.setResponse(response);
-		connection.deleteSms(query);
-		log(query);
+		// set verbosity, defaults to false
+		verbose = Boolean.parseBoolean(props.getProperty("verbose"));
 
-	}
+		// Random sillyness
+		if (verbose)
+			System.out.printf("%-30sDone.\n", "Corralling enough bits...");
 
-	/** Initializes the sms connection. */
-	public void initialize() throws ConnectionException {
-		connection.connect();
+		// set up pio text
+		System.out.printf("%-30s", "Initializing PioText...");
+		PioText piotxt = new PioText(props);
+		try {
+			piotxt.initialize();
+		} catch (ConnectionException e) {
+			System.out
+					.println("Could not establish sms connection. Exiting...");
+			e.printStackTrace();
+			System.exit(1);
+		}
+		System.out.println("Done.");
+
+		// Random sillyness
+		if (verbose)
+			System.out.printf("%-30sDone.\n\n", "Compacting widgets...");
+
+		// Run PioText
+		piotxt.run();
 	}
 
 }
