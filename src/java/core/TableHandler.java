dac7dbfb29ca7e6147d52e62eda4b50b8b0e78d5
Walt Javins <walt@javins.net>
2010-11-15 02:58:51 -0800

    Added service breaks to the TableHandler.
    For gaps of more than 120 minutes, the table handler will now
    display "(not in service)" as an entry.

--- src/java/core/TableHandler.java
+++ src/java/core/TableHandler.java
@@ -8,11 +8,21 @@ import persistance.schedule.Schedule;
 import persistance.schedule.Stop;
 import persistance.schedule.store.DefaultSchedule;
 import persistance.schedule.store.FileParser;
+import time.Day;
 import time.Time;
 import time.TimeRange;
 
 /** A message handler that performs a simple lookup in from a weekly schedule. */
 public class TableHandler extends MessageHandler {
+
+	/**
+	 * If there are more than this number of minutes between stops, then
+	 * BREAK_NOTICE will be inserted.
+	 */
+	private static final int GAP_MINUTES = 120;
+
+	/** The message that is displayed when there is a break in service. */
+	private static final String BREAK_NOTICE = "(not in service)";
 
 	private Schedule schedule;
 
@@ -42,12 +52,23 @@ public class TableHandler extends MessageHandler {
 		Iterable<Stop> stops = schedule.getNextStops(query.getKeyword(),
 				requestTime, 20);
 		// add a time for each stop
+		Time previousStopTime = Time.now();
 		for (Stop s : stops) {
-			String time = "\n" + s.getTime().toString(false);
+			Time currentStopTime = s.getTime();
+			// calculate if there is a break in Pio Express service
+			if (GAP_MINUTES < minutesBetween(previousStopTime, currentStopTime)) {
+				response.append("\n" + BREAK_NOTICE);
+			}
+			// don't exceed the boundries of one sms
+			String time = "\n" + currentStopTime.toString(false);
+			if (!s.getDirection().getMarker().isEmpty()) {
+				time += " " + s.getDirection().getMarker();
+			}
 			if (response.length() + time.length() > PioText.SMS_LENGTH) {
 				break;
 			} else {
 				response.append(time);
+				previousStopTime = currentStopTime;
 			}
 		}
 		return response.toString();
@@ -64,17 +85,37 @@ public class TableHandler extends MessageHandler {
 		TimeRange range = new TimeRange(requestTime, requestTime.addDays(1));
 		Iterable<Stop> stops = schedule.getStops(range);
 		// add a time @ location line for each stop
+		Time previousStopTime = Time.now();
 		for (Stop s : stops) {
-			String location = s.getKeyword();
-			Time time = s.getTime();
-			String line = String.format("\n%s @ %s", time.toString(), location);
+			Time currentStopTime = s.getTime();
+			// calculate if there is a break in Pio Express service
+			if (GAP_MINUTES < minutesBetween(previousStopTime, currentStopTime)) {
+				response.append("\n" + BREAK_NOTICE);
+			}
+			String location = keywords.getDefinition(s.getKeyword());
+			String line = String.format("\n%s @ %s",
+					currentStopTime.toString(), location);
+			// don't exceed the boundries of one sms
 			if (response.length() + line.length() > PioText.SMS_LENGTH) {
 				break;
 			} else {
 				response.append(line);
+				previousStopTime = currentStopTime;
 			}
 		}
 		return response.toString();
+	}
+
+	private long minutesBetween(Time begin, Time end) {
+		int days;
+		if (!begin.getDay().equals(end.getDay())) {
+			days = Day.daysBetween(begin.getDay(), end.getDay()).size() - 1;
+		} else {
+			days = 0;
+		}
+		int hours = end.getHour() - begin.getHour();
+		int minutes = end.getMinute() - begin.getMinute();
+		return 1440 * days + 60 * hours + minutes;
 	}
 
 }
